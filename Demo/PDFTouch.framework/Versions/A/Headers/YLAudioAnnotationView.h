027855c988ac113aaba3c202d02eff7ad5c6f101
Kemal Taskin <kemal.taskin@gmail.com>
2015-12-18 16:18:26 +0200

    Version 2.11

--- Demo/PDFTouch.framework/Versions/A/Headers/YLAudioAnnotationView.h
+++ Demo/PDFTouch.framework/Versions/A/Headers/YLAudioAnnotationView.h
@@ -7,13 +7,13 @@
 
 #import <UIKit/UIKit.h>
 #import "YLAnnotationView.h"
-#import <MediaPlayer/MediaPlayer.h>
+#import <AVKit/AVKit.h>
 
 /// View for displaying audio annotations.
 @interface YLAudioAnnotationView : UIView<YLAnnotationView>
 
-/// The MPMoviePlayerController used to play the audio file.
-@property (nonatomic, readonly) MPMoviePlayerController *audioPlayer;
+/// The AVPlayer used to play the audio file.
+@property (nonatomic, readonly) AVPlayer *audioPlayer;
 
 /// A Boolean value indicating wether audio playback should begin automatically.
 @property (nonatomic, assign) BOOL autostart;
